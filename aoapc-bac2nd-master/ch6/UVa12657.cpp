b638e6237b991400da73ed5a5a8a3c95d3a2bad4
zhangbo2008 <15122306087@163.com>
2021-02-04 16:18:27 +0800

    no_comment

--- aoapc-bac2nd-master/ch6/UVa12657.cpp
+++ aoapc-bac2nd-master/ch6/UVa12657.cpp
@@ -4,8 +4,8 @@
 #include<algorithm>
 using namespace std;
 
-const int maxn = 100000 + 5;
-int n, left[maxn], right[maxn];
+const int maxn = 100000 + 5;  //为什么加5.
+int n, left[maxn], right[maxn];  /// right表示下一个节点的index      //left表示上一个节点.
 
 inline void link(int L, int R) {
   right[L] = R; left[R] = L;
@@ -13,39 +13,59 @@ inline void link(int L, int R) {
 
 int main() {
   int m, kase = 0;
-  while(scanf("%d%d", &n, &m) == 2) {
+  while(scanf("%d%d", &n, &m) == 2) { //每次读2个数据,就处理开始.
+
+      //初始化链表.  0还是头指针.
     for(int i = 1; i <= n; i++) {
       left[i] = i-1;
-      right[i] = (i+1) % (n+1);
+      right[i] = (i+1) % (n+1);//最后节点连接到头结点. 当i取n时候, 连接到head节点.
     }
-    right[0] = 1; left[0] = n;
+
+
+    right[0] = 1; left[0] = n; //初始化.
     int op, X, Y, inv = 0;
 
-    while(m--) {
-      scanf("%d", &op);
-      if(op == 4) inv = !inv;
+
+
+
+    while(m--) {//读取m个操作.
+      scanf("%d", &op);   //每一个操作是一个数组. 读一个数字
+      if(op == 4) inv = !inv;  //进行翻转.
       else {
+
+
         scanf("%d%d", &X, &Y);
-        if(op == 3 && right[Y] == X) swap(X, Y);
-        if(op != 3 && inv) op = 3 - op;
+        if(op == 3 && right[Y] == X) swap(X, Y); //交换引用. 因为就是下一个,所以整个链表结构不变, 只改变数据即可. swap就够了.
+        if(op != 3 && inv) op = 3 - op; //因为之前有4操作互换.
         if(op == 1 && X == left[Y]) continue;
         if(op == 2 && X == right[Y]) continue;
 
         int LX = left[X], RX = right[X], LY = left[Y], RY = right[Y];
-        if(op == 1) {
+        if(op == 1) { // 如果是op1那么就
           link(LX, RX); link(LY, X); link(X, Y);
         }
         else if(op == 2) {
           link(LX, RX); link(Y, X); link(X, RY);
         }
-        else if(op == 3) {
+        else if(op == 3) {//如果是交换, 如果x本身就在y的左边.
           if(right[X] == Y) { link(LX, Y); link(Y, X); link(X, RY); }
           else { link(LX, Y); link(Y, RX); link(LY, X); link(X, RY); }
         }
+
+
+
       }
     }
 
-    int b = 0;
+
+
+
+
+
+
+
+//下面是打印结果.
+    int b = 0; //指针变量.
     long long ans = 0;
     for(int i = 1; i <= n; i++) {
       b = right[b];
@@ -53,6 +73,13 @@ int main() {
     }
     if(inv && n % 2 == 0) ans = (long long)n*(n+1)/2 - ans;
     printf("Case %d: %lld\n", ++kase, ans);
+
+
+
+
+
+
+
   }
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include<cstdio>
 #include<cstring>
 const int maxn = 100000 + 5;
-int last, cur, next[maxn]; // 光标位于cur号字符之后面//  next数组来实现的链表.
+int last, cur, next[maxn]; // 光标位于cur号字符之后面//  next数组来实现的链表.// 实际上是用next数组来 存光标跳转.
 char s[maxn];
 
 int main() {
     while(scanf("%s", s+1) == 1) {//存入数组中. 地址便宜一个.// 读入一个数据就存下来, 一个数据不超过100000+5个字符.
-        // 表示每次处理一个字符串, 空格作为结束符.
+        // 表示每次处理一个字符串, 空格作为结束符.   \000  在Windows系统中，正斜杠/表示除法， 除法是正统的运算, 用正斜杠表示--------用来进行整除运算；反斜杠\用来表示目录 反斜杠,反过来,所以是左撇子从左开始写.
 
         int n = strlen(s+1); // 输入保存在s[1], s[2]...中
         last = cur = 0;
@@ -16,18 +16,18 @@ int main() {
 
         for(int i = 1; i <= n; i++) {
             char ch = s[i];
-            if(ch == '[') cur = 0;
-            else if(ch == ']') cur = last;
+            if(ch == '[') cur = 0;  //cur表示走完当前字符后, 光标所在的位置.
+            else if(ch == ']') cur = last; // cur表示下个节点插入的位置.
             else {
-                next[i] = next[cur];
-                next[cur] = i;
-                if(cur == last) last = i; // 更新“最后一个字符”编号
-                cur = i; // 移动光标
+                next[i] = next[cur];  //遇到其他字符就正常跳转.----------这段逻辑等价于链表中的添加节点的操作. 这一行等价于 在 i-1节点和 i节点之间加入current. 也就是把i节点加入 i-1和current之间!!!!!!!!!!!!!!!!.    所以这个步奏是 把i的next指针指向cur
+                next[cur] = i;            // 表示current结点跳跃后的索引是i.
+                if(cur == last) last = i; // 更新“最后一个字符”编号,  用于更新上吗的else if 判断.
+                cur = i; // 移动光标   //current走到i结点. 做下次链接.
             }
         }
 
 
-        for(int i = next[0]; i != 0; i = next[i])
+        for(int i = next[0]; i != 0; i = next[i])// next找到下一个索引 ,  0是头结点.
             printf("%c", s[i]);
 
         printf("\n");
